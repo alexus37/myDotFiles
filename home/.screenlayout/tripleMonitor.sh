b57df5ac326f04d805e40fda79639b9a01e7f134
Alex Lelidis <alexander.lelidis@inf.ethz.ch>
2021-05-21 15:16:49 +0200

    add tablet commands

--- home/.screenlayout/tripleMonitor.sh
+++ home/.screenlayout/tripleMonitor.sh
@@ -8,8 +8,8 @@ echo "Setting out screen conv on hal 4"
     xrandr --output eDP-1-1 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output HDMI-1-2 --off --output DP-1-3 --off --output HDMI-1-3 --off --output DP-1-3-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-3-3 --off
 fi
 
-if [ "$curHostname" = "cgl-desktop-07.ethz.ch" ];
+if [ "$curHostname" = "hal5" ];
 then
     echo "cgl"
-    xrandr --output HDMI-0 --mode 1920x1200 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-0 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1-1 --off --output HDMI-1-2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1-1 --off
+    xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-0 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
 fi
---
+++ home/.scripts/tools/bindTabledToScreen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+screens=($(xrandr -q | grep " connected" | awk '{print $1}'))
+tabletId=$(xinput | grep "HID 256c:006e stylus" |  awk -F"id=" '{print $2}' | awk '{print $1}')
+
+if (( $1 < ${#screens[@]} )); then
+    echo "xinput map-to-output $tabletId ${screens[$1]}"
+    xinput map-to-output $tabletId ${screens[$1]}
+fi
+
